0147a6c48bbb3bb40718e9073e65de7c6168b3cf
Brady Sullivan <brady@bsull.com>
2019-01-26 17:03:05 -0800

    Add initial boltDB integration.

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/d1str0/HPFBroker
 
-require github.com/d1str0/hpfeeds v0.1.1
+require (
+	github.com/boltdb/bolt v1.3.1 // indirect
+	github.com/d1str0/hpfeeds v0.1.1
+	go.etcd.io/bbolt v1.3.0
+	golang.org/x/sys v0.0.0-20190121090251-770c60269bf0 // indirect
+)
--- main.go
+++ main.go
@@ -5,33 +5,64 @@ import (
 	"log"
 
 	"github.com/d1str0/hpfeeds"
+	bolt "go.etcd.io/bbolt"
 )
 
 const version = "v0.0.1"
+
+var path = "bolt.db"
 
 func main() {
 	fmt.Println("///- Starting up HPFBroker")
 	fmt.Printf("//- Version %s\n", version)
 
-	db := &IdentityDB{}
+	db, err := bolt.Open(path, 0666, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// Prepare DB and store as IdentityDB
+	idb, err := initializeDB(db)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	b := &hpfeeds.Broker{
 		Name: "HPFBroker",
 		Port: 10000,
-		DB:   db,
+		DB:   idb,
 	}
 	b.SetDebugLogger(log.Print)
 	b.SetInfoLogger(log.Print)
 	b.SetErrorLogger(log.Print)
 
-	err := b.ListenAndServe()
+	err = b.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-// TODO: Add a BoltDB instance here.
 type IdentityDB struct {
+	DB *bolt.DB
+}
+
+var BUCKETS = []string{
+	"identities",
+}
+
+// Initialize the database and assert certain buckets exist.
+func initializeDB(db *bolt.DB) (*IdentityDB, error) {
+	err := db.Update(func(tx *bolt.Tx) error {
+		for _, b := range BUCKETS {
+			_, err := tx.CreateBucketIfNotExists([]byte(b))
+			if err != nil {
+				return fmt.Errorf("create bucket: %s", err)
+			}
+		}
+		return nil
+	})
+	return &IdentityDB{DB: db}, err
 }
 
 // TODO: Replace this function with something that checks the DB for current identities.
