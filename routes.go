a142a69247f5795fed60c4756accd4750a1e52bc
Brady Sullivan <brady@bsull.com>
2019-02-09 20:44:44 -0800

    Working API calls for ident, GET, PUT, DELETE

--- routes.go
+++ routes.go
@@ -17,16 +17,51 @@ func statusHandler(app App) func(w http.ResponseWriter, r *http.Request) {
 
 func apiIdentHandler(app App) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
+		ident := r.URL.Path[len("/api/ident/"):]
+
+		// Handle API requests depending on HTTP Method
 		switch r.Method {
 		case http.MethodGet:
 			// Return identity if found
+			i, err := GetIdentity(app, ident)
+			buf, err := json.Marshal(i)
+			if err != nil {
+				http.Error(w, err.Error(), 500)
+			} else {
+				if i.Ident == "" {
+					http.Error(w, "Ident not found", 404)
+				} else {
+					fmt.Fprintf(w, "%s", buf)
+				}
+			}
 		case http.MethodPut:
 			// Update user
-		}
-		ident := r.URL.Path[len("/api/ident/"):]
-		i, err := GetIdentity(app, ident)
+			var id hpfeeds.Identity
+			if r.Body == nil {
+				http.Error(w, "Request body required", 400)
+				return
+			}
+			err := json.NewDecoder(r.Body).Decode(&id)
+			if err != nil {
+				http.Error(w, err.Error(), 400)
+				return
+			}
+			if ident != id.Ident {
+				http.Error(w, "Request body not valid for this URI. Ident mismatch.", 400)
+			}
 
-		fmt.Fprintf(w, "This is where you GET an asdfdent.\n%#v\n%#v)", i, err)
+			err = SaveIdentity(app, id)
+			if err != nil {
+				http.Error(w, err.Error(), 500)
+			}
+		case http.MethodDelete:
+			// Delete user
+			err := DeleteIdentity(app, ident)
+			if err != nil {
+				http.Error(w, err.Error(), 500)
+				return
+			}
+		}
 	}
 
 }
@@ -56,6 +91,15 @@ func SaveIdentity(app App, id hpfeeds.Identity) error {
 		buf, err := json.Marshal(id)
 		b.Put([]byte(id.Ident), buf)
 		return err
+	})
+	return err
+}
+
+func DeleteIdentity(app App, ident string) error {
+	err := app.DB.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("identities"))
+		b.Put([]byte(ident), nil)
+		return nil
 	})
 	return err
 }
