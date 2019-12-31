54c618e72b4675157cc50ee019c688eee6f057af
Brady Sullivan <brady@bsull.com>
2019-12-30 22:10:51 -0800

    Add a few comments to routes.go

--- routes.go
+++ routes.go
@@ -22,7 +22,6 @@ func statusHandler() func(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-//TODO return proper http codes per method
 func apiIdentDELETEHandler(bs BoltStore) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
@@ -175,7 +174,7 @@ func router(bs BoltStore) *mux.Router {
 	r := mux.NewRouter()
 	r.HandleFunc("/status", statusHandler())
 	r.HandleFunc("/api/ident/", apiIdentGETHandler(bs)).Methods("GET")
-	r.HandleFunc("/api/ident/", apiIdentPUTHandler(bs)).Methods("PUT")
+	r.HandleFunc("/api/ident/", apiIdentPUTHandler(bs)).Methods("PUT") // Funnel bad request for proper response.
 	r.HandleFunc("/api/ident/", apiIdentDELETEHandler(bs)).Methods("DELETE")
 	r.HandleFunc("/api/ident/{id}", apiIdentGETHandler(bs)).Methods("GET")
 	r.HandleFunc("/api/ident/{id}", apiIdentPUTHandler(bs)).Methods("PUT")
