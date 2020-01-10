875aa367be532fcd9391d862c16acdbfa24ed412
Brady Sullivan <brady@bsull.com>
2020-01-09 23:19:23 -0800

    Add User manager roles and super admin

--- auth/rbac.go
+++ auth/rbac.go
@@ -6,36 +6,54 @@ import (
 
 // Create separate read and write permissions
 var (
-	PermRead  = gorbac.NewStdPermission("read")
-	PermWrite = gorbac.NewStdPermission("write")
+	PermHPFRead  = gorbac.NewStdPermission("hpf_read")
+	PermHPFWrite = gorbac.NewStdPermission("hpf_write")
 
-	// Setup our main role
-	roleAdmin = "admin"
+	PermUserRead  = gorbac.NewStdPermission("user_read")
+	PermUserWrite = gorbac.NewStdPermission("user_write")
 
-	// These two are for testing primarily
-	roleReader = "reader"
-	roleWriter = "writer"
+	// Just for hpfeeds
+	roleHPFReader = "hpf_reader"
+	roleHPFAdmin  = "hpf_admin"
+
+	// Just for user management
+	roleUserReader = "user_reader"
+	roleUserAdmin  = "user_admin"
+
+	// Can control both
+	roleSuperAdmin = "super_admin"
 )
 
 // rbac returns a new instance of gorbac.RBAC for Role-Based Access Controls.
 func rbac() *gorbac.RBAC {
 	r := gorbac.New()
 
-	ra := gorbac.NewStdRole(roleAdmin)
-	rr := gorbac.NewStdRole(roleReader)
-	rw := gorbac.NewStdRole(roleWriter)
+	// Basic READ rights for HPFeeds
+	rhpfr := gorbac.NewStdRole(roleHPFReader)
+	rhpfr.Assign(PermHPFRead)
+	r.Add(rhpfr)
 
-	// Assign both permissions to admin role
-	ra.Assign(PermRead)
-	ra.Assign(PermWrite)
+	// Read and write for HPFeeds
+	rhpfa := gorbac.NewStdRole(roleHPFAdmin)
+	rhpfa.Assign(PermHPFRead)
+	rhpfa.Assign(PermHPFWrite)
+	r.Add(rhpfa)
 
-	rr.Assign(PermRead)
-	rw.Assign(PermWrite)
+	// Basic READ for HPFBroker users
+	rur := gorbac.NewStdRole(roleUserReader)
+	rur.Assign(PermUserRead)
+	r.Add(rur)
 
-	// Add role to rbac instance
-	r.Add(ra)
-	r.Add(rr)
-	r.Add(rw)
+	// Read and write for HPFBroker users
+	rua := gorbac.NewStdRole(roleUserAdmin)
+	rua.Assign(PermUserRead)
+	rua.Assign(PermUserWrite)
+	r.Add(rua)
+
+	// Super admin inherits both HPF Admin and User Admin
+	rsa := gorbac.NewStdRole(roleSuperAdmin)
+	r.Add(rsa)
+	r.SetParents(roleSuperAdmin, []string{roleUserAdmin, roleHPFAdmin})
 
 	return r
 }
--- auth/rbac_test.go
+++ auth/rbac_test.go
@@ -9,24 +9,44 @@ func Test_RBAC(t *testing.T) {
 
 	// Test "admin" role
 
-	if !r.IsGranted("admin", PermRead, nil) {
-		t.Error("Admin must be able to read")
+	if !r.IsGranted(roleHPFReader, PermHPFRead, nil) {
+		t.Error("HPF Reader must be able to read")
 	}
-	if !r.IsGranted("admin", PermWrite, nil) {
-		t.Error("Admin must be able to write")
-	}
-
-	if !r.IsGranted("reader", PermRead, nil) {
-		t.Error("Reader must be able to read")
-	}
-	if r.IsGranted("reader", PermWrite, nil) {
-		t.Error("Reader must NOT be able to write")
+	if r.IsGranted(roleHPFReader, PermHPFWrite, nil) {
+		t.Error("HPF Reader must not be able to write")
 	}
 
-	if r.IsGranted("writer", PermRead, nil) {
-		t.Error("Writer must NOT be able to read")
+	if !r.IsGranted(roleHPFAdmin, PermHPFRead, nil) {
+		t.Error("HPF Admin must be able to read")
 	}
-	if !r.IsGranted("writer", PermWrite, nil) {
-		t.Error("Writer must be able to write")
+	if !r.IsGranted(roleHPFAdmin, PermHPFWrite, nil) {
+		t.Error("HPF Admin must be able to write")
+	}
+
+	if !r.IsGranted(roleUserReader, PermUserRead, nil) {
+		t.Error("User Reader must be able to read")
+	}
+	if r.IsGranted(roleUserReader, PermUserWrite, nil) {
+		t.Error("User Reader must not be able to write")
+	}
+
+	if !r.IsGranted(roleUserAdmin, PermUserRead, nil) {
+		t.Error("User Admin must be able to read")
+	}
+	if !r.IsGranted(roleUserAdmin, PermUserWrite, nil) {
+		t.Error("User Admin must be able to write")
+	}
+
+	if !r.IsGranted(roleSuperAdmin, PermHPFRead, nil) {
+		t.Error("Super Admin must be able to read hpfeeds")
+	}
+	if !r.IsGranted(roleSuperAdmin, PermHPFWrite, nil) {
+		t.Error("Super Admin must be able to write hpfeeds")
+	}
+	if !r.IsGranted(roleSuperAdmin, PermUserRead, nil) {
+		t.Error("Super Admin must be able to read users")
+	}
+	if !r.IsGranted(roleSuperAdmin, PermUserWrite, nil) {
+		t.Error("Super Admin must be able to write users")
 	}
 }
