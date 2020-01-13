92d46d89645df2849c368060c7703d31c26b38c0
Brady Sullivan <brady@bsull.com>
2020-01-12 16:29:53 -0800

    Slight user test refactor.

--- user_test.go
+++ user_test.go
@@ -5,28 +5,32 @@ import (
 )
 
 func TestUser(t *testing.T) {
-	u, err := NewUser("name1", "pass1", "role1")
-	if err != nil {
-		t.Fatal(err)
-	}
+	t.Run("NewUser", func(t *testing.T) {
+		u, err := NewUser("name1", "pass1", "role1")
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	if u == nil {
-		t.Fatal("User should not be nil")
-	}
+		if u == nil {
+			t.Fatal("User should not be nil")
+		}
+	})
 
-	m, err := u.Authenticate("wrong")
-	if err != nil {
-		t.Fatal(err)
-	}
-	if m == true {
-		t.Error("Authentication should have failed")
-	}
-	
-	m, err = u.Authenticate("pass1")
-	if err != nil {
-		t.Fatal(err)
-	}
-	if m != true {
-		t.Error("Authentication should have succeeded")
-	}
+	t.Run("Authenticate", func(t *testing.T) {
+		m, err := u.Authenticate("wrong")
+		if err != nil {
+			t.Fatal(err)
+		}
+		if m == true {
+			t.Error("Authentication should have failed")
+		}
+
+		m, err = u.Authenticate("pass1")
+		if err != nil {
+			t.Fatal(err)
+		}
+		if m != true {
+			t.Error("Authentication should have succeeded")
+		}
+	})
 }
