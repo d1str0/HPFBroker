d5b5eb712596a1fffb74078d3d48b91eb534f952
Brady Sullivan <brady@bsull.com>
2020-02-13 15:15:12 -0800

    Move Permissions middleware into new file.

--- api/routes.go
+++ api/routes.go
@@ -5,46 +5,14 @@ import (
 
 	"fmt"
 	"net/http"
-	"strings"
 
 	"github.com/gorilla/mux"
-	rbac "github.com/mikespook/gorbac"
 )
 
 // TODO: Take a server object so we can display a version number.
 func statusHandler() func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "%s", "I'm online D:")
-	}
-}
-
-func (sc *ServerContext) Permission(p rbac.Permission, h http.HandlerFunc) func(w http.ResponseWriter, r *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		auth := strings.SplitN(r.Header.Get("Authorization"), " ", 2)
-		if len(auth) != 2 || auth[0] != "Bearer" {
-			// https://tools.ietf.org/html/rfc7235#section-3.1
-			w.Header().Set("WWW-Authenticate", "Basic")
-			http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
-			return
-		}
-
-		// auth[1] is the JWT at this point
-		claims, err := sc.JWTSecret.Validate(auth[1])
-		if err != nil {
-			// TODO: See what error might be returned, might not be good to
-			// divulge
-			w.Header().Set("WWW-Authenticate", "Basic")
-			http.Error(w, err.Error(), http.StatusUnauthorized)
-			return
-		}
-
-		role := claims["role"].(string)
-		if !sc.RBAC.IsGranted(role, p, nil) {
-			http.Error(w, http.StatusText(http.StatusForbidden), http.StatusForbidden)
-			return
-		}
-
-		h.ServeHTTP(w, r)
 	}
 }
 
