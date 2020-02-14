b9852a75d0ba06d4aa66dde4939eef54a1572edc
Brady Sullivan <brady@bsull.com>
2020-02-14 09:38:30 -0800

    Fix wrong package name in middleware_test.go

--- api/middleware_test.go
+++ api/middleware_test.go
@@ -1,10 +1,10 @@
-package main
+package api
 
 import (
 	"testing"
 )
 
 func TestPermission(t *testing.T) {
-	// Currently being tested via the User and Ident endpoints. 
+	// Currently being tested via the User and Ident endpoints.
 	// TODO: Add tests here for this middleware.
 }
